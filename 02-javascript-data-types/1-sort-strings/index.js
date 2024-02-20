fd32aae77c0ac86e47090f3c071a0e60a0da1ff6
NatSI <Silyukova1999@mail.ru>
2024-02-20 13:49:44 +0300

    Создана функция, возвращающая отсортированный массив согласно заданному порядку

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let resArr = [...arr];
+  switch (param) {
+    case 'asc':
+      return resArr.sort((word1, word2) => word1.localeCompare(word2, ['ru', 'en'], { caseFirst: "upper" }));
+    case 'desc':
+      return resArr.sort((word1, word2) => word2.localeCompare(word1, ['ru', 'en'], { caseFirst: "upper" }));
+  }
 }
