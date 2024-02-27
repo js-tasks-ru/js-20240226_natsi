3c9ac8ab9ddefcdfecf21541f760557158101fad
NatSI <Silyukova1999@mail.ru>
2024-02-27 21:45:39 +0300

    refactor(index.js) change const and remove switcher

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,11 +5,9 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-  let resArr = [...arr];
-  switch (param) {
-    case 'asc':
-      return resArr.sort((word1, word2) => word1.localeCompare(word2, ['ru', 'en'], { caseFirst: "upper" }));
-    case 'desc':
-      return resArr.sort((word1, word2) => word2.localeCompare(word1, ['ru', 'en'], { caseFirst: "upper" }));
+  const resArr = [...arr];
+  if (param === 'desc') {
+    return resArr.sort((word1, word2) => word2.localeCompare(word1, ['ru', 'en'], { caseFirst: "upper" }));
   }
+  return resArr.sort((word1, word2) => word1.localeCompare(word2, ['ru', 'en'], { caseFirst: "upper" }));
 }
--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,8 +5,8 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-    let newObject = {};
-    for (let key of Object.keys(obj)) {
+    const newObject = {};
+    for (const key of Object.keys(obj)) {
         if (fields.includes(key)) {
             newObject[key] = obj[key];
         }
