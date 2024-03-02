2ee5543c14861746efa5a9ec4a9655cef74873e9
NatSI <Silyukova1999@mail.ru>
2024-03-02 10:43:53 +0300

    task-3.4 create uniq function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,13 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+    let newArray = [];
+    if (Array.isArray(arr)) {
+        for (const item of arr) {
+            if (!newArray.includes(item)) {
+                newArray.push(item);
+            }
+        }
+    }
+    return newArray;
 }
