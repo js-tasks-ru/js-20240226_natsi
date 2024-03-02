1e05459f0abf38fcf6dbdcfed53a904435325cf8
NatSI <Silyukova1999@mail.ru>
2024-03-02 09:47:59 +0300

    task-3.2 create invertObj function

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (obj !== null && typeof obj === 'object') {
+        let newObj = {};
+        for (const key in obj) {
+            newObj[obj[key]] = key;
+        }
+        return newObj;
+    }
 }
