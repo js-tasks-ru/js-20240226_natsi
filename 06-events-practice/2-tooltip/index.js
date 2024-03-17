601baf84e241bbfc270697f4aaede64f64ee51e0
NatSI <Silyukova1999@mail.ru>
2024-03-17 12:18:06 +0300

    task-6.2 create Tooltip component

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,69 @@
 class Tooltip {
-  initialize () {
+  static instance;
+  element;
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+    Tooltip.instance = this;
+  }
 
+  createElement() {
+    const element = document.createElement('div');
+    element.classList.add('tooltip');
+    return element;
+  }
+
+  render(text = '') {
+    this.element.textContent = text;
+    document.body.append(this.element);
+  }
+
+  initialize () {
+    this.element = this.createElement();
+    this.setHandleListeners();
+  }
+
+  setHandleListeners() {
+    document.addEventListener('pointerover', this.documentPointeroverHandler);
+    document.addEventListener('pointerout', this.documentPointeroutHandler);
+    document.addEventListener('pointermove', this.documentPointermoveHandler);
+
+  }
+
+  removeHandleListeners() {
+    document.removeEventListener('pointerover', this.documentPointeroverHandler);
+    document.removeEventListener('pointerout', this.documentPointeroutHandler);
+    document.removeEventListener('pointermove', this.documentPointermoveHandler);
+
+  }
+
+  documentPointeroverHandler = (event) => {
+    if (event.target.dataset.tooltip) {
+      this.render(event.target.dataset.tooltip);
+    }
+  }
+
+  documentPointermoveHandler = (event) => {
+    if (event.target.dataset.tooltip) {
+      this.element.style.left = event.pageX + 'px';
+      this.element.style.top = event.pageY + 'px';
+    }
+  }
+
+  documentPointeroutHandler = (event) => {
+    if (event.target.dataset.tooltip) {
+      this.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.removeHandleListeners();
+  }
+
+  remove() {
+    this.element.remove();
   }
 }
 
